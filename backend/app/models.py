e5ae3d8dbf3587794caca0a33e3038220a0bf851
Grisky <grisky@icloud.com>
2026-01-18 20:09:08 +0300

    Chahge ph length

--- backend/app/models.py
+++ backend/app/models.py
@@ -8,7 +8,7 @@ class Member(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128))
+    password_hash = db.Column(db.String(512))
     role = db.Column(db.String(20), default='member')  
     join_at = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
 
