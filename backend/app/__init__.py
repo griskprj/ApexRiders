a8a0d4983d5624a3e16398c8d3d4e96cba944638
Grisky <grisky@icloud.com>
2026-01-18 21:44:04 +0300

    Change __init__ to VPS

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -24,7 +24,7 @@ def expired_token_loader(jwt_header, jwt_payload):
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://apexuser:8sE>HVx#~#*p;UM0D3LZ@localhost/apexriders'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://apexuser:f68bf603d97646ae2ffc4128b51b62bd@localhost:5432/apexriders'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['JWT_SECRET_KEY'] = 'secretKeyChangeToProduct'
     app.config['JWT_ACCESS_TOKEN_EXPIRES'] = datetime.timedelta(hours=24)
